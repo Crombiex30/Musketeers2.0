581f95cc0a20097bbdf8ac1ec49cc825b8447dd6
Carlos Garcia <carlos0garciaj@gmail.com>
2026-03-25 01:41:31 -0400

    So far combat
    Made some changes to the UI, currently working on the hud for the combat

---
+++ Musketeers2.0/Assets/Scripts/BattleSystem.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum BattleState{ START, PLAYERTURN, ENEMYTURN, WON, LOSS }
+public class BattleSystem : MonoBehaviour
+{
+
+    public GameObject playerPrefab;
+    public GameObject enemyPrefab;
+    public Text name; 
+
+    Unit playerUnit;
+    Unit enemyUnit;
+
+    public BattleState state;
+
+    void Start()
+    {
+        state = BattleState.START;
+        SetUpBattle();
+    }
+
+    void SetUpBattle()
+    {
+        GameObject playerGO = Instantiate(playerPrefab);
+        playerUnit = playerGO.GetComponent<Unit>();
+
+        GameObject enemyGO = Instantiate(enemyPrefab);
+        enemyUnit = enemyGO.GetComponent<Unit>();
+        
+        name.text = enemyUnit.unitName; 
+    }
+}
---
+++ Musketeers2.0/Assets/Scripts/Unit.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    
+    public string unitName;
+    public int unitLevel;
+    public int damage;
+    public int maxHP;
+    public int currentHP;
+}
